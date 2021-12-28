9d76d6aae08d2aa49ce9861e4abcf8d16c31e442
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2021-12-29 00:28:07 +0530

    RMX1921: RealmeParts: KcalService.java cleanup
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- RealmeParts/src/org/device/RealmeParts/kcal/KcalService.java
+++ RealmeParts/src/org/device/RealmeParts/kcal/KcalService.java
@@ -3,11 +3,7 @@ package org.device.RealmeParts.kcal;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.IBinder;
-import androidx.preference.PreferenceManager;
-
-import org.device.RealmeParts.kcal.DisplayCalibration;
 
 public class KcalService extends Service {
 
@@ -18,13 +14,13 @@ public class KcalService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-    Context context = getApplicationContext();
-    DisplayCalibration.restore(context);
-    return START_NOT_STICKY;
+        Context context = getApplicationContext();
+        DisplayCalibration.restore(context);
+        return START_NOT_STICKY;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-    return null;
+        return null;
     }
 }
