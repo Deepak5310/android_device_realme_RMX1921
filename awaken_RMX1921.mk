30203d3a6eecce174f475825cfa1872c1d75f46c
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2022-01-18 21:17:56 +0530

    RMX1921: Awaken-ify
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- awaken_RMX1921.mk
+++ awaken_RMX1921.mk
@@ -4,21 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common DerpFest stuff
+# Inherit some common Awaken stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-EXTRA_UDFPS_ANIMATIONS := true
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 # Official-ify
-DERP_BUILDTYPE := Official
+AWAKEN_BUILD_TYPE := OFFICIAL
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := derp_RMX1921
+PRODUCT_NAME := awaken_RMX1921
 PRODUCT_MODEL := Realme XT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
