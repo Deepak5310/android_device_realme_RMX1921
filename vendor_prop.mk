7196bece1ed3fe2fed4066b928d8eff408504b3a
Sushil Chauhan <sushilchauhan@codeaurora.org>
2022-01-19 02:00:36 +0530

    RMX1921: Enable H/W Composition 1. Enable H/W Composition. 2. Set number of FBSurface buffers to 3.
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -125,7 +125,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.egl.hw=0 \
+    debug.egl.hw=1 \
     debug.sf.hw=1 \
     ro.opengles.version=196610 \
     debug.renderengine.backend=skiaglthreaded \
