4eea1f83925b27ca9b1cb46677f8915ebe413440
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2022-08-22 19:48:13 +0530

    RMX1921: Update vendorsetup.sh
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,35 +1,4 @@
-# KERNEL TREE
-KERNEL="kernel/realme/RMX1921"
-if [ ! -d "$KERNEL" ]; then
 git clone --depth=1 https://github.com/kaderbava/android_kernel_realme_RMX1921.git -b 11 kernel/realme/RMX1921
-else
-  echo "No Need: ${KERNEL} is already available there."
-fi
-
-# VENDOR TREE
-VENDOR="vendor/realme/RMX1921"
-if [ ! -d "$VENDOR" ]; then
 git clone --depth=1 https://github.com/Parixshit/proprietary_vendor_realme_RMX1921.git -b 12 vendor/realme/RMX1921
-else
-  echo "No Need: ${VENDOR} is already available there."
-fi
-
-# CLANG NEUTRON-15
-NEUTRON="prebuilts/clang/host/linux-x86/clang-neutron"
-if [ ! -d "$NEUTRON" ]; then
-git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-15 prebuilts/clang/host/linux-x86/clang-neutron
-else
-  echo "No Need: ${NEUTRON} is already available there."
-fi
-
-# REALME DIRAC
-DIRAC="packages/apps/RealmeDirac"
-if [ ! -d "$DIRAC" ]; then
+git clone --depth=1 https://github.com/Neutron-Toolchains/neutron-clang.git prebuilts/clang/host/linux-x86/clang-neutron
 git clone --depth=1 https://github.com/HyperTeam/android_packages_apps_RealmeDirac.git -b lineage-19.0 packages/apps/RealmeDirac
-else
-  echo "No Need: ${DIRAC} is already available there."
-fi
-
-# OOS DIALER
-rm -rf packages/apps/Dialer
-git clone --depth=1 https://github.com/Parixshit/android_packages_apps_Dialer.git packages/apps/Dialer
